3dcd7bd7b59560b8c9ebfa2a243bd83ce949c07f
trknzmn <turkanozmnn@gmail.com>
2025-06-02 00:02:51 +0300

    Add files via upload

---
+++ anket.js
@@ -0,0 +1,15 @@
+
+//POP UP İÇİN//
+//const button=document.getElementById("buton");
+//const mainPopup=document.getElementById(".main-popup");
+
+//button.addEventListener('click',()=>{
+	
+  //  mainPopup.style.display='block';
+//})
+
+//BİLDİRİ VERME//
+function show() {
+    alert("Geri bildirimleriniz için teşekkür ederiz!");
+  }
+  
---
+++ site.js
@@ -0,0 +1,18 @@
+//TARAYICIDA SAAT GÖSTERME//
+const clock=document.querySelector('.clock');
+const tick = () =>{
+    const now =new Date();
+    const hours=now.getHours();
+    const minutes=now.getMinutes();
+    const seconds=now.getSeconds();
+
+   const html =
+   `
+   <span>${hours}</span> :
+   <span>${minutes}</span> :
+   <span>${seconds}</span> 
+   `
+   clock.innerHTML=html;
+};
+setInterval(tick, 10);
+
